7ae8f2cfee8534cb025dd59a7346b3e4b8e6cab7
Paul Adrian <67833935+alcoranpaul@users.noreply.github.com>
2024-10-15 02:49:33 -0500

    Created a custom window

--- Plugins/DunGen/Source/DunGen/DunGen.cs
+++ Plugins/DunGen/Source/DunGen/DunGen.cs
@@ -12,14 +12,7 @@ namespace DunGen
 	/// <seealso cref="GamePlugin" />
 	public class DunGen : GamePlugin
 	{
-		public PathFinding Pathfinding { get; private set; }
-		public DungeonGenSettings Settings { get; private set; }
-		public List<Room> Rooms;
-		private Actor DungeonGenActor;
-		private const string ACTOR_NAME = "DungeonGenActor";
-
-		public DungeonGenState State { get; private set; }
-		public GeneratorState GeneratorState { get; private set; }
+		public Generator Generator { get; private set; }
 		/// <inheritdoc />
 		public DunGen()
 		{
@@ -43,15 +36,9 @@ namespace DunGen
 		{
 			base.Initialize();
 			Debug.Log("DunGen Initialize");
-			var settings = Engine.GetCustomSettings("DunGenSettings");
-			if (!settings) Debug.LogError("DunGen does not exists");
-
-			Settings = settings.CreateInstance<DungeonGenSettings>();
-			State = DungeonGenState.None;
-			GeneratorState = GeneratorState.None;
-
-			Rooms = new List<Room>();
-
+			if (Generator.Instance == null)
+				Generator = new Generator();
+			else Generator = Generator.Instance;
 
 		}
 
@@ -59,281 +46,17 @@ namespace DunGen
 		public override void Deinitialize()
 		{
 			// Use it to cleanup data
-			Rooms = null;
 			base.Deinitialize();
 		}
 
 		public void GenerateDungeon()
 		{
-			// Spawn an empty actor to hold the dungeon
-			DungeonGenActor = Level.FindActor(ACTOR_NAME);
-			if (DungeonGenActor == null)
-			{
-				Debug.Log($"DunGen actor not found");
-				DungeonGenActor = new EmptyActor();
-				DungeonGenActor.Name = ACTOR_NAME;
-				Level.SpawnActor(DungeonGenActor);
-
-			}
-			else
-			{
-				Debug.Log($"DunGen actor found");
-			}
-			Debug.Log("Generating dungeon...");
-			ChangeState(DungeonGenState.Generating);
-
-			// Setup Grid plus Pathfinding
-			Pathfinding = new PathFinding(new Vector2(Settings.Size, Settings.Size));
-			Settings.BoundingBox = Pathfinding.GetBoundingBox();
-
-			DestroyDungeon();
-			SpawnRooms();
-			GenerateHallwayPaths();
-
-			ChangeGeneratorState(GeneratorState.None);
-			ChangeState(DungeonGenState.None);
-			Debug.Log("Dungeon generation complete");
-
-		}
-
-		private void DestroyDungeon()
-		{
-			ChangeState(DungeonGenState.Destroying);
-			if (Rooms.Count > 0)
-			{
-				// Iterate through each room and set it to null
-				for (int i = 0; i < Rooms.Count; i++)
-				{
-
-					GridSystem.GridPosition gridPos = Pathfinding.GridSystem.GetGridPosition(Rooms[i].WorldPosition.Position3D);
-					Pathfinding.ToggleNeighborWalkable(gridPos, Rooms[i].Width, Rooms[i].Length, true);
-					Rooms[i] = null;  // Set the room reference to null
-				}
-
-				// Now clear the list itself
-				Rooms.Clear();  // Remove all items from the list
-			}
-
-			// If Actor has children, destroy them
-			if (DungeonGenActor != null && DungeonGenActor.ChildrenCount > 0)
-			{
-				DungeonGenActor.DestroyChildren();
-			}
-
-		}
-
-		private void SpawnRooms()
-		{
-			ChangeGeneratorState(GeneratorState.SpawningRooms);
-			for (int i = 0; i < Settings.MaxRooms; i++)
-			{
-				GenerateRoom(out Room newRoom);
-				Rooms.Add(newRoom);
-			}
-		}
-
-		private void GenerateRoom(out Room newRoom)
-		{
-			Random rand = new Random();
-
-
-			int Width = rand.Next(2, 5);
-			int Height = rand.Next(1, 2);
-			int Length = rand.Next(2, 5);
-
-			bool isPositionValid = FindValidRoomPosition(Width, Height, Length, out Vector3 position);
-
-			if (!isPositionValid) Debug.LogError("No valid position found for room"); // Generate another room? until room count has reached max
-
-			Actor childModel = PrefabManager.SpawnPrefab(Settings.DebugSetting.RoomPrefab, position, Quaternion.
-			Identity);
-
-			childModel.Parent = DungeonGenActor;
-			childModel.Scale = new Vector3(Width, Height, Length);
-			StaticModel model = childModel as StaticModel;
-			model.SetMaterial(0, Settings.DebugSetting.Material);
-
-			GridSystem.GridPosition gridPos = Pathfinding.GridSystem.GetGridPosition(position);
-			Pathfinding.ToggleNeighborWalkable(gridPos, Width, Length, false);
-
-			Vector3 worldPos = Pathfinding.GridSystem.GetWorldPosition(gridPos);
-			RoomPosition roomPosition = new RoomPosition(worldPos);
-			newRoom = new Room(roomPosition, Width, Height, Length, childModel);
-
-			return;
-		}
-
-		private bool FindValidRoomPosition(int Width, int Height, int Length, out Vector3 _position)
-		{
-
-			return FindValidRoomPosition(Width, Height, Length, 5, out _position);
-		}
-
-		private bool FindValidRoomPosition(int Width, int Height, int Length, int maxAttemps, out Vector3 _position)
-		{
-			if (maxAttemps <= 0)
-			{
-				_position = Vector3.Zero;
-				return false;
-			}
-
-			// Create a random generator
-			Random rnd = new Random();
-
-			// Generate a random position inside the bounding box
-			Vector3 position = new Vector3(
-				rnd.Next((int)Settings.BoundingBox.Minimum.X, (int)Settings.BoundingBox.Maximum.X),
-				0, // Keep Y constant at 0 for floor placement
-				rnd.Next((int)Settings.BoundingBox.Minimum.Z, (int)Settings.BoundingBox.Maximum.Z)
-			);
-
-			// Check neighbors including base position
-			List<GridSystem.GridPosition> neightborhood = Pathfinding.GetNeighborhood(Pathfinding.GridSystem.GetGridPosition(position), Width, Length);
-			bool canOccupySpace = true;
-			foreach (var neighbor in neightborhood)
-			{
-				PathFinding.PathNode node = Pathfinding.GetNode(neighbor);
-				// Debug.Log($"Checking neighbor at {neighbor} with node is not null: {node != null}");
-
-				if (node == null) continue;
-				// Debug.Log($"Node is: {node}");
-				if (!node.IsWalkable)
-				{
-					canOccupySpace = false;
-					break;
-				}
-			}
-
-
-			// If there is no hit, set the position
-			if (canOccupySpace)
-			{
-				_position = Pathfinding.GridSystem.GetConvertedWorldPosition(position);
-				return true;
-			}
-
-			return FindValidRoomPosition(Width, Height, Length, --maxAttemps, out _position);
-
-		}
-
-		private void GenerateHallwayPaths()
-		{
-			ChangeGeneratorState(GeneratorState.GeneratingPaths);
-			float debugTime = 60f;
-			List<DelaunayTriangulation.Point> points = new List<DelaunayTriangulation.Point>();
-			HashSet<DelaunayTriangulation.Edge> edges = CreateDelaunayTriangulation(points);
-
-			List<DelaunayTriangulation.Edge> hallwayPaths = CalculatePaths(debugTime, points, edges);
-
-			// Set Node type to hallway nodes
-			foreach (var edge in hallwayPaths)
-			{
-				GridSystem.GridPosition startingPos = Pathfinding.GridSystem.GetGridPosition(edge.A.VPoint);
-				GridSystem.GridPosition end = Pathfinding.GridSystem.GetGridPosition(edge.B.VPoint);
-
-				Pathfinding.GetNode(startingPos).NodeType = PathFinding.NodeType.Hallway;
-				Pathfinding.GetNode(end).NodeType = PathFinding.NodeType.Hallway;
-			}
-
-			foreach (var edge in hallwayPaths)
-			{
-				GridSystem.GridPosition startingPos = Pathfinding.GridSystem.GetGridPosition(edge.A.VPoint);
-				GridSystem.GridPosition end = Pathfinding.GridSystem.GetGridPosition(edge.B.VPoint);
-				// DebugDraw.DrawSphere(new BoundingSphere(Pathfinding.GridSystem.GetWorldPosition(startingPos), 15f), Color.AliceBlue, 60f);
-				// DebugDraw.DrawSphere(new BoundingSphere(Pathfinding.GridSystem.GetWorldPosition(end), 15f), Color.Aqua, 60f);
-				List<GridSystem.GridPosition> paths = Pathfinding.FindPath(startingPos, end);
-				if (paths == null) continue;
-
-				for (int i = 0; i < paths.Count - 1; i++)
-				{
-					Actor floor1 = PrefabManager.SpawnPrefab(Settings.DebugSetting.FloorPrefab, Pathfinding.GridSystem.GetWorldPosition(paths[i]), Quaternion.Identity);
-					Actor floot2 = PrefabManager.SpawnPrefab(Settings.DebugSetting.FloorPrefab, Pathfinding.GridSystem.GetWorldPosition(paths[i + 1]), Quaternion.Identity);
-					floor1.Parent = DungeonGenActor;
-					floot2.Parent = DungeonGenActor;
-					DebugDraw.DrawLine(
-						Pathfinding.GridSystem.GetWorldPosition(paths[i]),
-						Pathfinding.GridSystem.GetWorldPosition(paths[i + 1]),
-						Color.Red,
-						60f
-					);
-				}
-			}
-		}
-
-		private List<DelaunayTriangulation.Edge> CalculatePaths(float debugTime, List<DelaunayTriangulation.Point> points, HashSet<DelaunayTriangulation.Edge> edges)
-		{
-			// Debug.Log("Calculating MST ...");
-			List<Prim.Edge> weightedEdges = new List<Prim.Edge>();
-			foreach (var edge in edges)
-			{
-				Prim.Edge e = new Prim.Edge(edge.A, edge.B);
-				weightedEdges.Add(e);
-				// DebugDraw.DrawText($"{e.Distance}", (edge.A.VPoint + edge.B.VPoint) / 2, Color.DarkRed, 8, debugTime, 0.5f);
-			}
-
-			List<DelaunayTriangulation.Edge> finalPaths = Prim.MinimumSpanningTree(weightedEdges, points[0]);
-
-			// Debug.Log($"Adding more paths into MST ..." + finalPaths.Count);
-			// Add more edges to the MST
-			foreach (var edge in edges)
-			{
-				if (finalPaths.Contains(edge)) continue;
-
-				float rand = Random.Shared.NextFloat();
-				if (rand < 0.451f)
-					finalPaths.Add(edge);
-			}
-
-			DelaunayTriangulation.Edge.DebugEdges(finalPaths, Color.DarkBlue, 40f);
-
-			return finalPaths;
-		}
-
-		private HashSet<DelaunayTriangulation.Edge> CreateDelaunayTriangulation(List<DelaunayTriangulation.Point> points)
-		{
-			foreach (var room in Rooms)
-			{
-				DelaunayTriangulation.Point point = new DelaunayTriangulation.Point(room.WorldPosition.X, room.WorldPosition.Z);
-				points.Add(point);
-			}
-			DelaunayTriangulation delaunay = DelaunayTriangulation.Triangulate(points);
-
-			return delaunay.Edges;
-		}
-
-		private void ChangeState(DungeonGenState state)
-		{
-			State = state;
-		}
-
-		private void ChangeGeneratorState(GeneratorState state)
-		{
-			GeneratorState = state;
+			if (Generator != null)
+				Generator.GenerateDungeon();
 		}
 	}
 
 
 
-	public enum DungeonGenState
-	{
-		/// <summary>
-		/// Nothing or Success
-		/// </summary>
-		None,
-		Idle,
-		Generating,
-		Destroying,
-		Failed
-	}
 
-	public enum GeneratorState
-	{
-		/// <summary>
-		/// Nothing or Success
-		/// </summary>
-		None,
-		SpawningRooms,
-		GeneratingPaths,
-		Failed
-	}
 }
--- Plugins/DunGen/Source/DunGenEditor/DunGenWindow.cs
+++ Plugins/DunGen/Source/DunGenEditor/DunGenWindow.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using DunGen;
 using FlaxEditor;
 using FlaxEditor.CustomEditors;
 using FlaxEditor.CustomEditors.Elements;
 using FlaxEngine;
+using FlaxEngine.GUI;
 
 namespace DunGenEditor;
 
@@ -14,35 +16,142 @@ namespace DunGenEditor;
 /// </summary>
 public class DunGenWindow : CustomEditorWindow
 {
-	private TextBoxElement folderName;
+	private DungeonGenSettings settings;
 	private const string SETTINGS_NAME = "DunGenSettings";
 	public string SETTINGS_PATH_FOLDER = "Data";
 	public string SettingsPath => Path.Combine(Globals.ProjectContentFolder + "/" + SETTINGS_PATH_FOLDER, SETTINGS_NAME + ".json");
+
+	public bool EnableDebugDraw = false;
 	public override void Initialize(LayoutElementsContainer layout)
 	{
-
+		Debug.Log($"Dungeon Gen class is initialized: {Generator.Instance != null}");
+		LoadData();
 
 		layout.Label("Dungeon Generation (DunGen)", TextAlignment.Center);
 		layout.Space(20);
-		var settingsGroup = layout.Group("Settings");
 
-		var hp = layout.HorizontalPanel();
-		hp.ContainerControl.Height = 20f;
-		settingsGroup.AddElement(hp);
+		// Settings group - location of settings json data
+		var settingsGroup = layout.VerticalPanel();
 
-		var name = layout.Label($"Settings Folder:");
-		hp.AddElement(name);
-		folderName = layout.TextBox();
-		folderName.Text = SETTINGS_PATH_FOLDER;
-		hp.AddElement(folderName);
+		var settingNameHP = layout.HorizontalPanel();
+		settingNameHP.ContainerControl.Height = 20f;
+		settingsGroup.AddElement(settingNameHP);
 
+		settingNameHP.AddElement(CreateLabel(layout, $"Settings Name:"));
+		settingNameHP.AddElement(CreateTextBox(layout, SETTINGS_NAME, textboxEnabled: false));
+
+		var settingFolderHP = layout.HorizontalPanel();
+		settingFolderHP.ContainerControl.Height = 20f;
+		settingsGroup.AddElement(settingFolderHP);
+
+		settingFolderHP.AddElement(CreateLabel(layout, $"Settings Folder:"));
+		settingFolderHP.AddElement(CreateTextBox(layout, SETTINGS_PATH_FOLDER, tooltip: "The folder where the settings is located"));
+
+		var enabbleDebugHP = layout.HorizontalPanel();
+		enabbleDebugHP.ContainerControl.Height = 20f;
+		settingsGroup.AddElement(enabbleDebugHP);
+		enabbleDebugHP.AddElement(CreateLabel(layout, $"Settings Folder:"));
+		var enableDebugBox = layout.Checkbox("Enable Debug Draw");
+		enabbleDebugHP.AddElement(enableDebugBox);
+		enableDebugBox.CheckBox.StateChanged += ToggleDebugDraw;
+
+
+
+		// Buttons group - Load, Save, Generate
 		layout.Space(20);
-		var button = layout.Button("Click me", Color.Blue);
-		button.Button.Clicked += OnButtonClicked;
+		var loadButton = layout.Button("Load", Color.DarkGray, $"Load settings from {SettingsPath}");
+		loadButton.Button.TextColor = Color.Black;
+		loadButton.Button.TextColorHighlighted = Color.Black;
+		loadButton.Button.Bold = true;
+		loadButton.Button.Clicked += LoadData;
+		layout.Space(10);
+		var saveButton = layout.Button("Open", Color.DarkGray, $"Open settings file @ {SettingsPath}");
+		saveButton.Button.TextColor = Color.Black;
+		saveButton.Button.TextColorHighlighted = Color.Black;
+		saveButton.Button.Bold = true;
+		saveButton.Button.Clicked += OpenData;
+		layout.Space(10);
+
+		var generateButton = layout.Button("Generate Dungeon", Color.DarkRed);
+		generateButton.Button.Clicked += GenerateDungeon;
+
+		var destroyButton = layout.Button("Destroy Dungeon", Color.DarkRed);
+		destroyButton.Button.Clicked += DestroyDungeon;
+	}
+
+	private void ToggleDebugDraw(CheckBox box)
+	{
+		EnableDebugDraw = box.Checked;
+		Debug.Log($"Debug Draw is {EnableDebugDraw}");
+	}
+
+	private TextBoxElement CreateTextBox(LayoutElementsContainer layout, string textValue, string tooltip = "", bool textboxEnabled = true)
+	{
+		var retVal = layout.TextBox();
+		retVal.Text = textValue;
+		retVal.TextBox.Enabled = textboxEnabled;
+		if (!string.IsNullOrEmpty(tooltip))
+			retVal.TextBox.TooltipText = tooltip;
+		return retVal;
+	}
+
+	private LabelElement CreateLabel(LayoutElementsContainer layout, string name, string tooltip = "", bool textboxEnabled = true)
+	{
+		var retVal = layout.Label(name);
+		retVal.Label.Enabled = textboxEnabled;
+		if (!string.IsNullOrEmpty(tooltip))
+			retVal.Label.TooltipText = tooltip;
+		return retVal;
+	}
+
+	private void GenerateDungeon()
+	{
+		DebugDraw.UpdateContext(IntPtr.Zero, float.MaxValue);
+		Generator generator = new Generator();
+		generator.GenerateDungeon();
+
+		if (!EnableDebugDraw)
+			DebugDraw.UpdateContext(IntPtr.Zero, float.MaxValue);
+	}
+
+	private void DestroyDungeon()
+	{
+		DebugDraw.UpdateContext(IntPtr.Zero, float.MaxValue);
+		Actor DungeonGenActor = Level.FindActor("DungeonGenActor");
+		// If Actor has children, destroy them
+		if (DungeonGenActor != null && DungeonGenActor.ChildrenCount > 0)
+		{
+			DungeonGenActor.DestroyChildren();
+		}
 	}
 
 
-	private void OnButtonClicked()
+	private void LoadData()
+	{
+		var asset = Content.Load(SettingsPath);
+		if (asset == null) Debug.LogWarning($"Failed to load settings @ {SettingsPath}");
+		// TODO: Add auto create settings if a bool is enabled
+
+		if (asset is not JsonAsset) Debug.LogWarning($"Settings @ {SettingsPath} is not a JsonAsset");
+
+		JsonAsset json = asset as JsonAsset;
+		settings = json.CreateInstance<DungeonGenSettings>();
+
+
+	}
+
+	private void OpenData()
+	{
+		var asset = Content.Load(SettingsPath);
+		if (asset == null) Debug.LogWarning($"Failed to load settings @ {SettingsPath}");
+		// TODO: Add auto create settings if a bool is enabled
+
+		if (asset is not JsonAsset) Debug.LogWarning($"Settings @ {SettingsPath} is not a JsonAsset");
+		Editor.Instance.ContentEditing.Open(asset);
+	}
+
+
+	private void SaveData()
 	{
 
 
